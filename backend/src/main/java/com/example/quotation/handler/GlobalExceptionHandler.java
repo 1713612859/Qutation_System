d6edb060be5e623638fdd6b18d4289e123d80eef
Liuyunlong <1713612859@qq.com>
2025-11-27 12:37:23 +0800

    add brand & category

--- backend/src/main/java/com/example/quotation/handler/GlobalExceptionHandler.java
+++ backend/src/main/java/com/example/quotation/handler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
-package com.example.quotation.exception;
+package com.example.quotation.handler;
 
 import com.example.quotation.common.ApiResponse;
+import com.example.quotation.exception.ServiceException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
